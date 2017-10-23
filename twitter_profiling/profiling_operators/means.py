fixed precision problem
Signed-off-by: JoGreen <giorgiomaria@live.it>

twitter_profiling/profiling_operators/means.py
@@ -1,4 +1,4 @@
 import numpy as np
 def geometric_mean(iterable):
     a = np.array(iterable)
-    return a.prod()**(1.0/len(a))
+    return (a* 1.0).prod()**(1.0/len(a))