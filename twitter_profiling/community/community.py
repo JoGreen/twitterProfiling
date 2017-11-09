1b6f416d1c47c7e475b6597f7047fa3ea4716632
JoGreen <giorgiomaria@live.it>
2017-11-09 13:13:30 +0100

    forward clique semantic aggregation
    Signed-off-by: JoGreen <giorgiomaria@live.it>

--- twitter_profiling/community/community.py
+++ twitter_profiling/community/community.py
@@ -1,12 +1,23 @@
 from twitter_profiling.clique_profiling.clique import Clique
+import sys
+
+graph_cohesion_threeshold = 1.2
+vector_cohesion_threeshold = 0.008
+
 
 class Community(Clique):
 
     def __init__(self, users, id1, id2):
+        # type:(list[str], str, str)->None
         try:
             id1 = str(id1)
             id2 = str(id2)
         except:
-            print 'wron input type initializing Community instance'
+            print 'wrong input type initializing Community instance'
+            sys.exit(1)
         super(Community, self).__init__(users, id1+id2)
-
+        self.cliques = [id1, id2]
+        if self.get_vectors_cohesion() < vector_cohesion_threeshold:
+            self.accept_in_links = True
+        else:
+            self.accept_in_links = False
