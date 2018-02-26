7283eda0bd2e5ddb3e15036c5b01e53ea2edc7a5
JoGreen <giorgiomaria@live.it>
2018-02-26 18:22:59 +0100

    added parameter
    Signed-off-by: JoGreen <giorgiomaria@live.it>

--- community_detection/clique_profiling/parameters.py
+++ community_detection/clique_profiling/parameters.py
@@ -1,4 +1,8 @@
 
 
-twitter = {'k':1, "cohesion_threshold":0.018  }# used for twitter kaggle dataset
-acm = {'k':2, 'cohesion_threshold': 0.13, 'minimum_num_of_interests': 3 } # used for acm dataset 0.13
+twitter = {'k':1, "cohesion_threshold":0.018, "gt": 7  }# used for twitter kaggle dataset
+acm = {
+    'k':2,
+    'cohesion_threshold': 0.13, # used for acm dataset 0.13
+    'minimum_num_of_interests': 3,
+    "gt": 0 }
