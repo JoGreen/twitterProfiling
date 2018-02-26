2dabdc8930db036f5a89dfaffa76658585cc50eb
JoGreen <giorgiomaria@live.it>
2018-02-26 18:04:37 +0100

    added dependencies
    Signed-off-by: JoGreen <giorgiomaria@live.it>

--- setup.py
+++ setup.py
@@ -2,5 +2,15 @@ from setuptools import setup
 
 setup(name='community_detection',
       version='1.1',
-      install_requires=['networkx', 'pymongo', 'tweepy', 'progressbar', 'igraph', 'tqdm']
+      install_requires=[
+            'networkx',
+            'pymongo',
+            'tweepy',
+            'progressbar',
+            'igraph',
+            'tqdm',
+            'sklearn',
+            'scipy',
+            'matplotlib'
+      ]
       )
